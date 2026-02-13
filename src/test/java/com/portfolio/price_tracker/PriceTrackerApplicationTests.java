0c5b8b9e268330ed8e896a589ec193f5ca16269e
Paula <paausj@gmail.com>
2026-02-13 12:09:23 +0100

    Initial commit: proyect structure from Spring Initializr

---
+++ src/test/java/com/portfolio/price_tracker/PriceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.price_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
